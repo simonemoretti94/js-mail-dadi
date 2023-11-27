3a803c02b467005e76dd52eb71f83f96968bf9ff
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-27 20:47:16 +0100

    5th review with Luigi

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,8 +18,7 @@ const verification = document.querySelector('p#p_verification');
 
 //simone.moretti.944@gmail.com
 
-//declaring counter for utility purpose
-let c=  0;
+
 
 //button click listener
 button_click.addEventListener('click', function() {
@@ -27,31 +26,33 @@ button_click.addEventListener('click', function() {
     //verifying values received
     console.log('input: ',input_mail.value, ' button: ', button_click.innerHTML, ' p verification: ', verification.innerHTML);
 
+    //declaring counter for utility purpose
+    let c = -1;
+
     //iterating on the array
-    for (let i = 0; i <= (e_mails.length - 1); i ++) {
+    for (let i = 0; i < e_mails.length; i ++) {
         console.log('actual mail: ',e_mails[i]);
         
         //if in order to verify input ad array index
         if (input_mail.value === e_mails[i]) {
-            verification.innerHTML = `${input_mail.value} found at position #${i}`;
-        }
-        else {
-            // c = parseInt(c + 1);
-            c += 1;
-            console.log('c: ',c);
-            if (c === e_mails.length) {
-                verification.innerHTML = `${input_mail.value} not found`;
-            }
-            else {
-                continue;
-            }
+            c = i;
         }
     }
+    
+    // c = parseInt(c + 1);
+    console.log('c: ',c);
+    if (c > -1) {
+        verification.innerHTML = `${input_mail.value} found at position #${c}`;
+        
+    }
+    else {
+        verification.innerHTML = `${input_mail.value} not found`;   
+     }
+        
+        console.log('array length: ', e_mails.length, ' c:', c);
+    }
+    )
 
 
-})
 
-
-console.log('array length: ', e_mails.length, ' c:', c);
-
-
+// console.log(Math.floor(Math.random()*6 + 1));
