fdba636b8d70679c2d84a04b94f4bfb6c23ca7ab
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-27 17:22:41 +0100

    2nd commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 //script
 
+// mail verification 
+
+const e_mails = ['giovanni.rana@yahoo.it', 'simone.moretti.alto@gmail.com', 'maremma.maiala@aruba.com'];
+
+const input_mail = document.querySelector('input#mail_input');
+
+const button_click = document.querySelector('button#mail_submit');
+
+console.log('input: ',input_mail.innerHTML, ' button: ', button_click.innerHTML);
+
+
